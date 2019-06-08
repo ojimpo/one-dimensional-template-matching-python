2e309382f102c14a7f9134d2ce61a1c27c61b404
ojimpo <kou997@googlemail.com>
2019-06-08 15:37:24 +0900

    あ、なんか出来たかもしれない

--- easy.py
+++ easy.py
@@ -1,2 +1,10 @@
 data = [1, 1, 4, 5, 1, 4, 8, 1, 0, 1, 9, 1, 9]
 template = [8, 1, 0]
+
+# print(template)
+
+for i in range(0, len(data), len(template)):
+    scan = data[i:i+len(template)]
+    for j in range(len(scan)):
+        diff = scan[j] - template[j]
+        print(diff)
