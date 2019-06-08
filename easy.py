51902aac6246dd81f0df3ae4cd7a8479b4c76413
ojimpo <kou997@googlemail.com>
2019-06-08 16:15:33 +0900

    動くようにした

--- easy.py
+++ easy.py
@@ -1,10 +1,23 @@
+import numpy as np
+
 data = [1, 1, 4, 5, 1, 4, 8, 1, 0, 1, 9, 1, 9]
 template = [8, 1, 0]
 
 # print(template)
 
+diffScores = []
+
 for i in range(0, len(data), len(template)):
     scan = data[i:i+len(template)]
+    score = []
     for j in range(len(scan)):
-        diff = scan[j] - template[j]
+        diff = abs(scan[j] - template[j])
+        score.append(diff)
         print(diff)
+    diffScores.append(sum(score))
+
+print('min index is: ' + str(np.argmin(diffScores)))
+bestScoreIndex = len(template) * np.argmin(diffScores)
+print(bestScoreIndex)
+
+print(diffScores)
