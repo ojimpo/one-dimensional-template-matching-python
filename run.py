1280465796c4d53978543cd6c3941f1ac1d647b6
ojimpo <kou997@googlemail.com>
2019-06-08 16:42:27 +0900

    絶対値の差が最も少ない場所が求められた

--- run.py
+++ run.py
@@ -5,33 +5,23 @@ import numpy as np
 with open('l_knee_angles_formatted.csv') as f:
     data = f.read().splitlines()
 
-# data配列の一部を切り出してテンプレートとする
-# sliceを用いて切り出す
+# data配列の一部をliceを用いて切り出してテンプレートとする
 template = data[30:60]
 
-# 元データ配列, テンプレート配列の要素数を取得
-dlength = len(data)
-tlength = len(template)
-
-#元データの一番後ろを取得
-dlast = data[-1]
+# 配列内の要素の型をint型に変換する
+data = [float(i) for i in data]
+template = [float(i) for i in template]
 
 # スコア格納用の配列を作成
-# 一番小さな値のインデックスがテンプレートと一致する位置
-score = []
+diffScores = []
 
-# テンプレートと同じ長さのデータを, 元データの最初から最後まで順番に取り出していく
-calcdata = []
-for calc in range(dlength):
-    calcdata.append(data[calc])
-    if len(calcdata) == tlength:
-        break
-for i in template:
-    print(i)
-# 取り出したものとテンプレート間で, すべての値の絶対値の差を求める
-# 全ての桁の差を足し合わせ、それをスコアとする
-# スコアが一番小さかった部分が一致度が最も高い. そのインデックスを出力する
+for i in range(0, len(data), 1):
+    scan = data[i:i+len(template)]
+    score = []
+    for j in range(len(scan)):
+        diff = abs(scan[j] - template[j])
+        score.append(diff)
+    diffScores.append(sum(score))
 
-print(calcdata)
-print(template)
-print(data[-1])
+print('min score is: ' + str(np.min(diffScores)) + ', min index is: ' + str(np.argmin(diffScores)))
+print(diffScores)
